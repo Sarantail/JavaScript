d840ee12c1940e002fa1160bf6cc35a12afb0cd3
sarantail <sarantail@gmail.com>
2023-07-29 17:55:40 +0300

    new commit

--- 5/script.js
+++ 5/script.js
@@ -1,45 +1,134 @@
 // Задание 1
-// Создайте функцию которая возводит переданное число в куб, необходимо вывести в консоль результат 2^3 степени + 3 ^ 3 степени
-const cubing = (num) => num ** 3;
-console.log(cubing(2) + cubing(3));
+// Дан объект numbers. Необходимо в консоль вывести все значения больше или равные 3.
+
+const numbers = {
+keyin1: 1,
+keyin2: 2,
+keyin3: 3,
+keyin4: 4,
+keyin5: 5,
+keyin6: 6,
+keyin7: 7,
+}
+for (const key in numbers) {
+    if (numbers[key] >= 3) {
+        console.log(numbers[key]);
+    }
+}
+
 
 // Задание 2
-// Пользователь вводит с клавиатуры число, если ввёл текст, необходимо вывести что значение задано неверно
-// Создать фукнцию, которая высчитывает 13% от данного числа и выводит в консоль текст "Размер заработной платы за вычетом налогов равен значение"
-let salaryUser = Number(prompt('Введите число'));
+// Необходимо из объекта, который лежит в константе post вывести значения, к которым приписан комментарий, в консоль.
 
-const tax = (salary) => salary - (salary * 0,13);
+const post = {
+author: "John", // вывести этот текст
+postId: 23,
+comments: [
+{
+    userId: 10,
+    userName: "Alex",
+    text: "lorem ipsum",
+    rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+    },
+},
+{
+    userId: 5, // вывести это число
+    userName: "Jane",
+    text: "lorem ipsum 2", // вывести этот текст
+    rating: {
+            likes: 3,
+            dislikes: 1,
+            },
+        },
+    ],
+};
+console.log(post.author);
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
 
-if (typeof salaryUser === 'string') {
-    console.log('Значение задано не верно');
-} else {
-    console.log(`Размер заработной платы за вычетом налогов равен ${tax(salaryUser)}`);
-}
+
 
 // Задание 3
-// Пользователь с клавиатуры вводит 3 числа, необходимо создать функцию, которая определяет максимальное значение среди этих чисел
-let number1 = Number(prompt('Введите число 1'));
-let number2 = Number(prompt('Введите число 2'));
-let number3 = Number(prompt('Введите число 3'));
+// Дан массив products, необходимо цену каждого продукта уменьшить на 15% используя метод forEach.
 
-const maxNumber = (num1, num2, num3) => Math.max(num1, num2, num3);
-console.log(maxNumber(number1, number2, number3));
+const products = [
+{
+    id: 3,
+    price: 200,
+},
+{
+    id: 4,
+    price: 900,
+},
+{
+    id: 1,
+    price: 1000,
+},
+];
+products.forEach(item => {
+    item.price -= (15 * item.price / 100);
+});
+console.log(products);
+
 
 // Задание 4
-// Необходимо реализовать четыре функции, каждая функция должна принимать по два числа и выполнять одну из операций (каждая функция выполняет одну из них):
-// 1. Сложение
-// 2. Разность
-// 3. Умножение
-// 4. Деление
-// Необходимо сделать так, чтобы функция вернула число, например выражение console.log(sum(2, 6));
-// должно вывести число 8 в консоль(sum - функция сложения в данном примере, ваши названия функций могут отличаться).
-// Округлять значения, которые возвращают функции не нужно, однако, обратите внимание на разность,
-// функция должна вычесть из большего числа меньшее, либо вернуть 0, если числа равны.
-// Функциям всегда передаются корректные числа, проверки на NaN, Infinity делать не нужно.
-const addition = (num1, num2) => num1 + num2;
-const difference = (num1, num2) => {
-    if (num1 === num2) return 0;
-    return (num1 > num2) ? num1 - num2 : num2 - num1;
-}
-const multiplication = (num1, num2) => num1 * num2;
-const division = (num1, num2) => num1 / num2;
+// 1. Необходимо вывести в консоль массив продуктов в котором есть хоть одна фотография используя метод filter. Исходные данные - массив products.
+// 2. Необходимо отсортировать массив products используя метод sort по цене, начиная с самой маленькой, 
+// заканчивая самой большой ценой, после чего вывести отсортированный массив в консоль.
+
+
+const products2 = [
+{
+    id: 3,
+    price: 127,
+    photos: [
+        "1.jpg",
+        "2.jpg",
+    ],
+},
+{
+    id: 5,
+    price: 499,
+    photos: [],
+},
+{
+    id: 10,
+    price: 26,
+    photos: [
+        "3.jpg",
+],
+},
+{
+    id: 8,
+    price: 78,
+},
+];
+const newProducts = products2.filter(item => {
+            if (item.photos != false) return item.photos;
+        })
+        console.log(newProducts);
+
+        const newProducts2 = products2.sort((a, b) => {
+            return a.price - b.price;
+        }
+        );
+        console.log(newProducts2);
+
+
+
+// Задание 5
+// Дано 2 массива 
+
+// Вам необходимо объединить 2 этих массива,
+// чтобы значения первого массива были ключами, а значения второго массива — значениями.
+
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+const obj = {};
+for (let i = 0; i < en.length; i++) {
+    obj[en[i]] = ru[i];
+        }
+        console.log(obj);
